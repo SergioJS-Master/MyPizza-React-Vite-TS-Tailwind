7b28b5e17a1d26bf32d081a93758b60dc3cd939b
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-10-11 18:22:15 +0300

    add routing in progect

---
+++ app/src/Components/Pages/DetailPizza/DetailPizza.tsx
@@ -0,0 +1,7 @@
+export function DetailPizza () {
+    return (
+        <div>
+            detailPizza
+        </div>
+    )
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Main } from './Components/Pages/Main/Main.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { FormAddPizzas } from './Components/Pages/FormAddPizzas/FormAddPizzas.tsx'
+import { DetailPizza } from './Components/Pages/DetailPizza/DetailPizza.tsx'
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Main />,
+        },
+        {
+          path: 'menu',
+          element: <FormAddPizzas />
+        },
+        {
+          path: 'menu/:detailPizzaId',
+          element: <DetailPizza />
+        }
+      ],
+    },
+  ],
+  // { basename: '/DogShopReact/' },
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
