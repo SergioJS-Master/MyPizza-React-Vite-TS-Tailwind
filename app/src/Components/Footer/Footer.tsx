1bd078c436c28c02a09b79faae75ef1ffc85c804
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-10-12 23:26:31 +0300

    added a form for adding pizza

--- app/src/Components/Footer/Footer.tsx
+++ app/src/Components/Footer/Footer.tsx
@@ -1,5 +1,15 @@
 export function Footer () {
     return (
-        <div>FOOTER</div>
+        <div className="bg-[#D81921] flex justify-around items-center">
+            <div>
+                <h1 className="text-[24px] text-white">MyPizza</h1>
+            </div>
+            <div>
+                <a className="text-[24px] text-white" href="https://github.com/SergioJS-Master">GitHub</a>
+            </div>
+            <div>
+                <h1 className="text-[24px] text-white">2023 г.</h1>
+            </div>
+        </div>
     )
 }
