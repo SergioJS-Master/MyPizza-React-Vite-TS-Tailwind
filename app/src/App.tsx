4b01205f594feca7ad1da95a5c2364158be86f64
SergioJS-Master <Sergio.Krapchini@yandex.ru>
2023-10-17 19:59:30 +0300

    created an array to which pizzas are added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,10 +3,15 @@ import './App.css'
 import { Header } from './Components/Header/Header'
 import { Footer } from './Components/Footer/Footer'
 import { FC } from 'react'
+// import Pizza from './models/Pizza'
 
 
 const App: FC = () => {
+  // const [pizzasList, setPizzasList] = useState<Pizza[]>([])
 
+  // const addPizza = (newPizza: Pizza) => {
+  //   setPizzasList([...pizzasList, newPizza])
+  // }
 
   return (
     <>
